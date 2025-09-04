b91c4901ee9746e06f2cd45489afe49375366fd6
msolal <maelys.solal@cs.ox.ac.uk>
2025-09-04 14:44:32 +0200

    comment out simulation mask save

--- clinicadl/utils/task_manager/task_manager.py
+++ clinicadl/utils/task_manager/task_manager.py
@@ -250,14 +250,14 @@ class TaskManager:
                         label = data["label"][idx].squeeze(0).cpu()
                         reconstruction = outputs["recon_x"][idx].squeeze(0).cpu()
                         input_nii = nib.Nifti1Image(image[0].numpy(), eye(4))
-                        sim_nii = nib.Nifti1Image(label.numpy(), eye(4))
+                        # sim_nii = nib.Nifti1Image(label.numpy(), eye(4))
                         output_nii = nib.Nifti1Image(reconstruction.numpy(), eye(4))
                         # Create file name according to participant and session id
                         input_filename = f"{participant_id}_{session_id}_image_input.nii.gz"             
-                        sim_filename = f"{participant_id}_{session_id}_image_sim.nii.gz"
+                        # sim_filename = f"{participant_id}_{session_id}_image_sim.nii.gz"
                         output_filename = f"{participant_id}_{session_id}_image_output.nii.gz"
                         nib.save(input_nii, path.join(nifti_path, input_filename))
-                        nib.save(sim_nii, path.join(nifti_path, sim_filename))
+                        # nib.save(sim_nii, path.join(nifti_path, sim_filename))
                         nib.save(output_nii, path.join(nifti_path, output_filename))
               
                     # Save latent tensor
