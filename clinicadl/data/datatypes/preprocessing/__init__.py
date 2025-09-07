5f3dcf3374da51b3681c921fb29dcd36642438e1
msolal <maelys.solal@icm-institute.org>
2025-09-07 12:59:36 +0200

    PET skull stripping preprocessing

--- clinicadl/data/datatypes/preprocessing/__init__.py
+++ clinicadl/data/datatypes/preprocessing/__init__.py
@@ -4,4 +4,5 @@ from .dti import DWIDTI
 from .factory import get_preprocessing_config
 from .flair import FlairLinear
 from .pet import PETLinear
+from .pet_skull_stripped import PETLinearSkullStripped
 from .t1w import T1Linear
